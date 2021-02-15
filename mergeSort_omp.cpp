cf83c7a8ffb7e5da6b82c73154594e17e783bc65
waterfog9580 <waterfog9580@gmail.com>
2021-02-15 16:54:53 +0900

    not yet

--- mergeSort_omp.cpp
+++ mergeSort_omp.cpp
@@ -1,88 +1,32 @@
-#include<iostream>
-#include<stdlib.h>
-#include<sys/time.h>
-#include<omp.h>
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <omp.h>
+#include <sys/time.h>
 
-#define MAX_SIZE 10000
+#define SIZE 10000
+#define THREAD 1
+#define SWAP(a,b) {int temp = a; a = b; b = temp;}
 
 using namespace std;
 
-int temp_list[MAX_SIZE];
+void init_List(int arr[]);
+void merge(int arr[],int size, int temp[]);
+void mergeSort(int arr[],int size, int temp[]);
+void mergeSort_omp(int arr[],int size, int temp[], int threads);
 
-void get_randArr(int (&arr)[MAX_SIZE]);
-void merge(int list[], int left, int mid, int right);
-void mergeSort_parallel(int list[], int left, int right,int threads);
-void mergeSort_parallel_internal(int list[], int left, int right);
-
-void get_randArr(int (&arr)[MAX_SIZE]){
-	srand((unsigned int)time(NULL));
-	for (int i = 0; i < MAX_SIZE; i++)
-	{
-		arr[i] = rand() % MAX_SIZE;
-	}
-}
-
-void merge(int list[], int left, int mid, int right){
-    int i, j, k, l;
-    i = left;
-    j = mid +1;
-    k = left;
-
-    /*merge divided list*/
-    while(i<=mid && j<=right){
-        if(list[i]<= list[j])
-            temp_list[k++] = list[i++];
-        else
-            temp_list[k++] = list[j++];
-    }
-
-    /*Copy the remaining values*/
-    if(i>mid){
-        for(l=j; l<=right; l++)
-            temp_list[k++] = list[l];
-    }
-    else{
-        for(l=i; l<=mid; l++)
-            temp_list[k++] = list[l];
-    }
-
-    /*copy temp list to main list*/
-    for(l=left; l<=right; l++){
-        list[l] = temp_list[l];
-    }
-}
-
-void mergeSort_parallel(int list[], int left, int right,int threads){
-    #pragma omp parallel num_threads(threads)
-    {
-        #pragma omp single
-        mergeSort_parallel_internal(list,left,right);
-    }
-}
-
-void mergeSort_parallel_internal(int list[], int left, int right){
-    int mid;
-    if(left<right){
-        mid = (left+right)/2;
-        #pragma omp task
-        mergeSort_parallel_internal(list, left, mid);
-        #pragma omp task
-        mergeSort_parallel_internal(list, mid+1, right);
-
-        merge(list, left, mid, right);
-    }
-}
-
-int main(){
+int main(int argc,char* argv[]){
     float sum = 0;
-    int list[MAX_SIZE];
+    int list[SIZE];
+    int temp[SIZE];
+
 	for (int i = 0; i < 10000; i++)
 	{
 		struct timeval stime,etime,gap;
-		get_randArr(list);
+		init_List(list);
 		
 		gettimeofday(&stime,NULL);
-    	mergeSort_parallel(list,0, MAX_SIZE-1, 1);
+    	mergeSort_omp(list,SIZE,temp, THREAD);
        	gettimeofday(&etime,NULL);
 
        	gap.tv_sec = etime.tv_sec - stime.tv_sec;
@@ -95,4 +39,63 @@ int main(){
        	sum += gap.tv_usec;
     }
     cout << sum/10000 << endl;
+}
+
+void init_List(int arr[]){
+    srand((unsigned int)time(NULL));
+	for (int i = 0; i < SIZE; i++)
+        arr[i] = rand() % SIZE;
+}
+
+void merge(int arr[], int size, int temp[]) {
+	int i1 = 0;
+	int i2 = size / 2;
+	int it = 0;
+
+	while(i1 < size/2 && i2 < size) {
+		if (arr[i1] <= arr[i2])
+			temp[it] = arr[i1++];
+		else
+			temp[it] = arr[i2++];
+		it++;
+	}
+
+	while (i1 < size/2)
+	    temp[it++] = arr[i1++];
+	while (i2 < size)
+	    temp[it++] = arr[i2++];
+
+	memcpy(arr, temp, size*sizeof(int));
+}
+
+void mergeSort(int arr[],int size, int temp[]){
+    int i;
+
+	if (size == 2) { 
+		if (arr[0] <= arr[1])
+			return;
+		else {
+			SWAP(arr[0], arr[1]);
+			return;
+		}
+	}
+
+	mergeSort(arr, size/2, temp);
+	mergeSort(arr + size/2, size - size/2, temp);
+	merge(arr, size, temp);
+}
+
+void mergeSort_omp(int arr[],int size, int temp[], int threads){
+    if(threads = 1)
+        mergeSort(arr,size,temp);
+    else if(threads > 1){
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            mergeSort_omp(arr, size/2, temp, threads/2);
+            #pragma omp section
+            mergeSort_omp(arr+size/2, size-size/2,temp+size/2, threads-threads/2);
+        }
+    }
+    merge(arr, size, temp);
 }
